4a01596b3f052dab11c2d3b0edea94201f6a0e7c
TruptiTotanalli <truptitotanalli@gmail.com>
2026-02-18 15:07:47 +0530

    jobs

--- src/test/java/Testcases/LoginTest.java
+++ src/test/java/Testcases/LoginTest.java
@@ -2,9 +2,11 @@ package Testcases;
 
 import org.testng.annotations.Test;
 
+
 import Baseclass.BaseTest;
 import POM.LoginTestpage;
 import utilities.DataProviderClass;
+import utilities.Log;
 
 public class LoginTest extends BaseTest{
 
@@ -14,6 +16,8 @@ public class LoginTest extends BaseTest{
 	    LoginTestpage loginPage = new LoginTestpage(getDriver());
 	   
 	    loginPage.login(user, pass);
+	    Log.logger.info("checking jenkins job");
+	    Log.logger.info("connection between github and jenkins");
 	}
 	
 }
